01bc87cfce506681d7d8b340a5fb72f3f73e57da
Jake Holtz <jahholtz@gmail.com>
2017-12-02 18:23:29 -0800

    Updated structure and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppCountryComponent } from './app.country.component.js';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppCountryComponent
   ],
   imports: [
     BrowserModule
